e0bca603d249f01e965d1e48072d0129b18f54e2
Kunal Pandey <kunalpandeykumar2004@gmail.com>
2025-04-03 17:05:49 +0000

    data types

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,10 @@
+const accountId = 2344
+let accountEmail ="soocan.com"
+var accountPassword ="fksnck"
+accountCity = "jaipur"
+console.log(accountId);
+
+// accountEmail = "hchfgmail.com"
+// accountPassword = 55652
+// accountCity = "bihar"
+console.table([ accountId, accountEmail, accountPassword, accountCity]);
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,23 @@
+"use strict";// treat all js code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+console.log(3+ 3)//code readability should be high
+// ECMA is use for looking through javascript standard.
+console.log("Kunal")
+
+let name = "Kunal"
+let age ="20"
+let isLoggedIn = false
+
+// number =>2 to power 53
+//bigint
+//string =>  ""
+//boolean => true/false
+//null => standalone value 
+//undefined =>
+//symbol =>unique
+
+//object
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
