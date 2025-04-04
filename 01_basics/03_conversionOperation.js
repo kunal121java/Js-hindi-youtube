0dc41009624ac68d5d07fc003b193a7c8aca5685
Kunal Pandey <kunalpandeykumar2004@gmail.com>
2025-04-04 14:37:07 +0000

    Conversion of data type

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score ="30"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+let valueInNumber = Number(score)
+//console.log(typeof  valueInNumber);
+//console.log(valueInNumber);
+
+//"30"=>30
+// "30abc" => NaN
+//true =>1; false =>0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//1 => true; 0 => false
+// "" => false
+// "kunal" =>true
