55174b001400eeb80e9c97372d3a62f01423e95a
Kunal Pandey <kunalpandeykumar2004@gmail.com>
2025-04-13 16:44:09 +0000

    data type summary

---
+++ 01_basics/datatype_summay.js
@@ -0,0 +1,31 @@
+//Primitive
+
+//7 types : String , Number,Boolean, null,undefined, Symbol,BigInt
+
+
+//Reference type(Non-Primitive)
+
+//Array, Objects ,Functions (Important toppic for js)
+const outsideTemp = null
+
+const id= Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id===anotherId);
+const bignumber = 354535364646535464n
+
+//Array , Object, Function
+
+const heros = ["saktiman","nagraj","doga"];
+
+let myobj = {
+     name:"Kunal",
+     age : 20,
+
+}
+const myfunction = function(){
+    console.log("hello world");
+    
+}
+console.log(typeof bignumber);
+
