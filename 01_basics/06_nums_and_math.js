d2fab09bd4a6c4f6f4019f327e482dbf5d785fd1
Kunal Pandey <kunalpandeykumar2004@gmail.com>
2025-04-27 02:22:20 +0000

    number and maths

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,50 @@
+const score  = 400
+//console.log(score);
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));
+
+//const otherNumber = 123.869
+
+//console.log(otherNumber.toPrecision(4));
+// Use very care fully toPreciosion Function.
+
+// toLocalestring method is use to make easy to calculate large number of zero
+
+const hundreds =10000000
+//console.log(hundreds.toLocaleString('en-IN'));
+//We use basically 'en-IN' for conerting them into indian standard and (the default value  is en-USA)
+
+
+//++++++++++++++++++++++ MATHS +++++++++++++++++++++
+console.log(Math);
+console.log(Math.abs(-4));
+// its gives absolute value
+console.log(Math.round(4.6));
+// its give the round off value
+console.log(Math.ceil(4.3));
+// gives the ceil value
+console.log(Math.floor);
+console.log((Math.random()*10)+1);
+
+// its give the floor value
+/* Math.abs(x): Returns the absolute value of x.
+Math.round(x): Returns x rounded to the nearest integer. 
+Math.ceil(x): Returns x rounded up to the nearest integer. 
+Math.floor(x): Returns x rounded down to the nearest integer. 
+Math.trunc(x): Returns the integer part of x.
+Math.sqrt(x): Returns the square root of x.
+Math.pow(x, y): Returns x to the power of y. 
+Math.min(x, y, ...): Returns the smallest of its arguments.
+Math.max(x, y, ...): Returns the largest of its arguments.
+Math.random(): Returns a pseudo-random number between 0 (inclusive) and 1 (exclusive).  */
+
+
+
+
+
+
+
